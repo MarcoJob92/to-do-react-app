2f1413f7febae1518e13ff025fb3ef63031fef74
Marco Furone <marco.job92@gmail.com>
2019-05-18 21:45:30 +0100

    Minor changes

--- src/components/Select.js
+++ src/components/Select.js
@@ -4,13 +4,13 @@ export default function Select(props) {
   	return(
 	  	<select name={props.name} 
 	  	        value={props.value}
-	            onChange={props.onChange}
-	            className="form-control">
+	                onChange={props.onChange}
+	                className="form-control">
 	        
 	        <option value="">Please, select</option>
 	        {props.options.map(opt => 
 	        	<option value={opt} key={opt}> {opt} </option>
 	        )}
 		</select>
-    )
+         )
 }
