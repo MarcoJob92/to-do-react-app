dc3fbcc3a82499d00f5a004df0d5efc215e72570
Marco Furone <marco.job92@gmail.com>
2019-05-18 21:44:45 +0100

    Minor changes

--- src/components/Select.js
+++ src/components/Select.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Select(props) {
   	return(
 	  	<select name={props.name} 
-	  	        value={props.assigned}
+	  	        value={props.value}
 	            onChange={props.onChange}
 	            className="form-control">
 	        
